38bb0873ee349b18b526c4457a11440a8329f34e
Nirmali <nirmalichristy@gmail.com>
2024-10-11 17:00:24 +0530

    first

---
+++ CourseManagementSystem_v2/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    internal class Course
+    {
+       public string courseId {  get; set; }
+           public string title { get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
